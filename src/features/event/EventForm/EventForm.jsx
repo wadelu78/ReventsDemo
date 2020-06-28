352921da8e767934aa2bd12ec2cf6c10275cfc6d
Wei Lu <wadelu0714@gmail.com>
2020-06-28 13:27:24 +1200

    End of section 5

--- src/features/event/EventForm/EventForm.jsx
+++ src/features/event/EventForm/EventForm.jsx
@@ -10,9 +10,21 @@ export class EventForm extends Component {
     hostedBy: '',
   }
 
+  componentDidMount() {
+    if (this.props.selectedEvent !== null) {
+      this.setState({
+        ...this.props.selectedEvent,
+      })
+    }
+  }
+
   handleFormSubmit = (e) => {
     e.preventDefault()
-    this.props.createEvent(this.state)
+    if (this.state.id) {
+      this.props.updateEvent(this.state)
+    } else {
+      this.props.createEvent(this.state)
+    }
   }
 
   handleInputChange = ({ target: { name, value } }) => {
